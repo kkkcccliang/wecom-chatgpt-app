a05567d275375d73a56694a815ce9686cdfc23ed
liangjingzhan <fatzhan@163.com>
2023-05-10 08:31:37 +0800

    multiple keys

--- src/openai/openai.ts
+++ src/openai/openai.ts
@@ -3,18 +3,51 @@ import fs from 'fs'
 import chatCache from './cache'
 import { config } from '../config'
 
-const configuration = new Configuration({
-    apiKey: config.openaiApiKey,
-    basePath: config.openaiApiUrl,
+const openAIClients = config.openaiApiKeys.split(',').map((key: string) => {
+    return new OpenAIApi(new Configuration({
+        apiKey: key,
+        basePath: config.openaiApiUrl,
+    }))
 })
-const openai = new OpenAIApi(configuration)
+
+let activeIndex = 0
+const getActiveClient = (next = false) => {
+    if (next) {
+        console.info('try next client, index:' + activeIndex)
+        activeIndex += 1
+        if (activeIndex >= openAIClients.length) {
+            activeIndex = 0
+        }
+    }
+    return openAIClients[activeIndex]
+}
+
+// 超过请求次数则不再重试
+let tooManyTimeout: string | number | NodeJS.Timeout | undefined
+let retrys = 0
+const isTooManyRequests = () => {
+    return tooManyTimeout && retrys >= openAIClients.length
+}
+const markTooManyRequests = () => {
+    if (!tooManyTimeout) {
+        tooManyTimeout = setTimeout(resetTooManyRequests, 30000)
+    }
+    retrys++
+}
+const resetTooManyRequests = () => {
+    if (tooManyTimeout) {
+        clearTimeout(tooManyTimeout)
+        retrys = 0
+        tooManyTimeout = undefined
+    }
+}
 
 /**
  * Get completion from OpenAI
  * @param username
  * @param message
  */
-async function chatgpt(username: string, message: string): Promise<string> {
+async function chatgpt(username: string, message: string, next?: boolean): Promise<string> {
     const messages = chatCache.getChatMessages(username)
     messages.push({
         role: ChatCompletionRequestMessageRoleEnum.User,
@@ -22,16 +55,17 @@ async function chatgpt(username: string, message: string): Promise<string> {
     })
     let response: any
     try {
-        response = await openai.createChatCompletion({
+        response = await getActiveClient(next).createChatCompletion({
             model: config.openaiModel,
             messages: messages,
             temperature: config.openaiTemperature,
         })
-    } catch (e) {
+    } catch (e: any) {
         if (e?.response?.statusText) {
             console.error('openai error:' + e.response.statusText)
-            if (e.response.statusText === 'Too Many Requests') {
-                // TODO
+            if (e.response.statusText === 'Too Many Requests' && !isTooManyRequests()) {
+                markTooManyRequests()
+                return await chatgpt(username, message, true)
             }
             return e.response.statusText
         }
@@ -45,6 +79,7 @@ async function chatgpt(username: string, message: string): Promise<string> {
             // 请求成功后加入对话上下文
             chatCache.addUserMessage(username, message)
             chatCache.addAssistantMessage(username, assistantMessage)
+            resetTooManyRequests()
         } else {
             const err = `Something went wrong, status: ${response.status}, ${response.statusText}`
             console.error(err)
@@ -64,7 +99,7 @@ async function chatgpt(username: string, message: string): Promise<string> {
  * @param prompt
  */
 async function dalle(username: string, prompt: string): Promise<string> {
-    const response = await openai
+    const response = await getActiveClient()
         .createImage({
             prompt: prompt,
             n: 1,
@@ -75,7 +110,7 @@ async function dalle(username: string, prompt: string): Promise<string> {
         .then(res => res.data)
         .catch(err => console.log(err))
     if (response) {
-        return response.data[0].url
+        return response.data[0].url as string
     } else {
         return 'Generate image failed'
     }
@@ -88,7 +123,7 @@ async function dalle(username: string, prompt: string): Promise<string> {
  */
 async function whisper(username: string, videoPath: string): Promise<string> {
     const file: any = fs.createReadStream(videoPath)
-    const response = await openai
+    const response = await getActiveClient()
         .createTranscription(file, 'whisper-1')
         .then(res => res.data)
         .catch(err => console.log(err))
